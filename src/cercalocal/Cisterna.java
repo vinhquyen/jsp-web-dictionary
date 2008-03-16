8136f85356deaf619de67f39b2926bb7e9a8cc79
b8047247 <b8047247@05d4d382-69db-11de-bdd1-8505e4066020>
2008-03-16 22:18:45 +0000

    Crea CercaLocal practice

---
+++ src/cercalocal/Cisterna.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cercalocal;
+
+/**
+ *
+ * @author chiron
+ */
+public class Cisterna {
+private int x;
+private int y;
+
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
